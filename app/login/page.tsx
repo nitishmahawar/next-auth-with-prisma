187aaeed91fdd764b936f05fce8ef21f19537fdf
Nitish Mahawar <nitish.mahawar@apttechsols.com>
2023-07-24 13:10:08 +0530

    new commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
-  const { data: session } = useSession();
   return (
     <div className="min-h-screen w-full flex items-center justify-center flex-col gap-4">
       <h2 className="text-2xl font-medium">Next Auth Login</h2>
-      <pre>{JSON.stringify(session, undefined, 2)}</pre>
       <Button
         variant="outline"
         onClick={() => signIn("google", { callbackUrl: "/" })}
@@ -17,7 +15,6 @@ const LoginPage = () => {
         <FcGoogle size={20} className="mr-4" />
         Sign In with Google
       </Button>
-      <Button onClick={() => signOut()}>Logout</Button>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const IndexPage = () => {
-  return <div></div>;
+  const { data: session, status } = useSession();
+  return (
+    <div className="px-4 py-6 space-y-6">
+      <h1 className="text-2xl font-semibold">Protected Page</h1>
+      {session ? (
+        <div>
+          <p>{session.user?.name}</p>
+          <p>{session.user?.email}</p>
+          <img src={session.user?.image!} alt="" />
+        </div>
+      ) : (
+        <Loader2 size={20} className="animate-spin" />
+      )}
+
+      <Button onClick={() => signOut()}>Logout</Button>
+    </div>
+  );
 };
 
 export default IndexPage;
