d8d465026bcbea460657d52be3bcd246cc8b1f41
Nitish Mahawar <nitish.mahawar@apttechsols.com>
2023-07-24 13:19:51 +0530

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 const IndexPage = () => {
   const { data: session, status } = useSession();
@@ -11,15 +12,28 @@ const IndexPage = () => {
     <div className="px-4 py-6 space-y-6">
       <h1 className="text-2xl font-semibold">Protected Page</h1>
       {session ? (
-        <div>
-          <p>{session.user?.name}</p>
-          <p>{session.user?.email}</p>
-          <img src={session.user?.image!} alt="" />
+        <div className="flex items-center gap-x-2">
+          <Image
+            className="object-cover w-10 h-10 rounded-full"
+            src={session.user?.image!}
+            height={100}
+            width={100}
+            alt="image"
+          />
+
+          <div>
+            <h1 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+              {session.user?.name}
+            </h1>
+
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              {session.user?.email}
+            </p>
+          </div>
         </div>
       ) : (
         <Loader2 size={20} className="animate-spin" />
       )}
-
       <Button onClick={() => signOut()}>Logout</Button>
     </div>
   );
